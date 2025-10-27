4d229c99fbaa3a4007c4070ebfe577b7786cf676
proshehab <backup.files4466@gmail.com>
2025-10-27 11:38:22 +0600

    update

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,17 +1,28 @@
 import { useState } from 'react';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
+  const submit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ name, email, password, confirmPassword });
+  }
 
   return (
     <>
           <div className="space-y-4 max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
             <h2 className="text-2xl font-bold mb-6 text-gray-800 text-center">Register</h2>
-            <form className="space-y-4">
+            <form onSubmit={submit} className="space-y-4">
               <div>
                 <label className="block font-bold text-gray-700">Name</label>
                 <input
-                  type="text"
+                  id='neme'
+                  name="name"
+                  value ={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                   placeholder="Enter your name"
                 />
@@ -19,7 +30,10 @@ export default function Register() {
                <div>
                 <label className="block font-bold text-gray-700">Email</label>
                 <input
-                  type="text"
+                  id ='email'
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                   placeholder="Enter your email"
                 />
@@ -27,7 +41,11 @@ export default function Register() {
                <div>
                 <label className="block font-bold text-gray-700">Password</label>
                 <input
+                  id='password'
+                  name="password"
                   type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                   placeholder="Enter your password"
                 />
@@ -35,12 +53,16 @@ export default function Register() {
                <div>
                 <label className="block font-bold text-gray-700">Confirm Password</label>
                 <input
+                  id='confirmPassword'
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                   type="password"
                   className="mt-1 block w-full border border-gray-300 rounded-md p-2"
                   placeholder="Confirm your password"
                 />
               </div>
-            </form>
+           
             <button
               type="submit"
               className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200"
@@ -50,6 +72,7 @@ export default function Register() {
 
                <div className="flex items-center justify-between mt-4">
           </div>
+           </form>
           </div>
         </>
       );
