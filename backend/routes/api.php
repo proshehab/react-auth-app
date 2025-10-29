b571ca349505239a213eb7eea13819cc12cc8b17
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2025-10-29 22:35:27 +0600

    add profile route and create ProfileController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,3 +11,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
